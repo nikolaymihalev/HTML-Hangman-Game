e92c4c4dc51c0cdbf4d8b1b35b880b557699c79c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-02 08:56:50 +0000

    Implement function for listening keyboard

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ for(let i = 97; i <= 122; i++){
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);    
+    button.addEventListener("click",e=>initGame(e.target,String.fromCharCode(i)));
 }
 
 getRandomWord();
