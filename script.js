026d775dc09a589be48653f09fb4ba5415539734
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-05 12:38:51 +0000

    Create game over function

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const wordDisplay = document.querySelector(".word-display");
 const keyboardDiv = document.querySelector(".keyboard");
 const guessText = document.querySelector(".guesses-text b");
 const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
 
 const maxGuesses = 6;
 
 let currentWord;
 let wrongGuessCount = 0;
+let correctLetters = [];
+
+const gameOver = (isVictory)=>{
+    setTimeout(()=>{
+        gameModal.classList.add("show");
+    },300);
+}
 
 const getRandomWord = () => {
     const {word, hint} = wordList[Math.floor(Math.random()*wordList.length)];
@@ -19,6 +27,7 @@ const initGame = (button, clickedLetter)=>{
     if(currentWord.includes(clickedLetter)){
         [...currentWord].forEach((letter, index)=>{
             if(letter===clickedLetter){
+                correctLetters.push(letter);
                 wordDisplay.querySelectorAll("li")[index].innerText = letter;
                 wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
             }
@@ -27,7 +36,11 @@ const initGame = (button, clickedLetter)=>{
         wrongGuessCount++;
         hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
     }
+    button.disabled = true;
     guessText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    if(wrongGuessCount===maxGuesses) return gameOver(false);
+    if(correctLetters.length===currentWord.length) return gameOver(true);
 }
 
 for(let i = 97; i <= 122; i++){
